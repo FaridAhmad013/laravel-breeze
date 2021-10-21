1da55007968da4223262b561f7a237ca29902eeb
FaridAhmad013 <lilisrohanah13@gmail.com>
2021-10-21 13:48:04 +0700

    Menambahkan Validasi

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <x-slot name="title">Home</x-slot>
+    <x-slot name="header">
+        <h2 class="font-semi-bold text-xl text-gray-800 leading-tight">
+            {{ __('Home') }}
+        </h2>
+    </x-slot>
+</x-app-layout>
