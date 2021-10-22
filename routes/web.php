d5415869eca31744075cb90037f3b27912b8a4d7
FaridAhmad013 <lilisrohanah13@gmail.com>
2021-10-22 08:27:26 +0700

    verifikasi email

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::view('/home', 'home')->name('home');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::view('/home', 'home')->name('home');
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
